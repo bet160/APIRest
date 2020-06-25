662bbeec3df13f3838e553806aa833a35ed202dd
bet160 <36347319+bet160@users.noreply.github.com>
2020-06-25 16:04:52 -0500

    Proyecto C#
    Proyecto API REST

---
+++ ApiRestCuenta/Entidad/Cuenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestCuenta.Entidad
+{
+    public class Cuenta
+    {
+        [Key]
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public string contraseña { get; set; }
+        public string nombre { get; set; }
+        public bool tieneAcceso { get; set; }
+    }
+}
